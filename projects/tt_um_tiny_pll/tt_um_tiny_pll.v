a7d6df5bc84595d471994cac223ccfd2e7e2f7db
TinyTapeoutBot <139130078+TinyTapeoutBot@users.noreply.github.com>
2024-09-03 06:38:35 +0300

    feat: update project tt_um_tiny_pll from LegumeEmittingDiode/tt08-tiny-pll
    Commit: 19bbd043e3c7a86fff8350dc46f2b46d647f4acd
    Workflow: https://github.com/LegumeEmittingDiode/tt08-tiny-pll/actions/runs/10676137531

---
+++ projects/tt_um_tiny_pll/tt_um_tiny_pll.v
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2024 Your Name
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+`default_nettype none
+
+module tt_um_tiny_pll (
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
+);
+
+endmodule
