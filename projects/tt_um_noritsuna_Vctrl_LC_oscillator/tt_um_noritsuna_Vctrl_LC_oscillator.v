78ed567cc1fce36d4bd507cee76765e70f3fddf0
TinyTapeoutBot <139130078+TinyTapeoutBot@users.noreply.github.com>
2024-09-03 13:53:43 +0300

    feat: update project tt_um_noritsuna_Vctrl_LC_oscillator from noritsuna/tt08-analog-Vctrl_LC_oscillator
    Commit: bda8d3bc4cd4be22ef38c428a129a6d48fd9c8b1
    Workflow: https://github.com/noritsuna/tt08-analog-Vctrl_LC_oscillator/actions/runs/10681504569

---
+++ projects/tt_um_noritsuna_Vctrl_LC_oscillator/tt_um_noritsuna_Vctrl_LC_oscillator.v
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2024 Your Name
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+`default_nettype none
+
+module tt_um_noritsuna_Vctrl_LC_oscillator (
+    input  wire       VGND,
+    input  wire       VDPWR,    // 1.8v power supply
+//    input  wire       VAPWR,    // 3.3v power supply
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    inout  wire [7:0] ua,       // Analog pins, only ua[5:0] can be used
+    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
+);
+
+endmodule
