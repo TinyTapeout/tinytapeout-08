3b22a1b949de8bd02c7c33f1fa405f8c5f5bc5e4
TinyTapeoutBot <139130078+TinyTapeoutBot@users.noreply.github.com>
2024-09-05 17:57:10 +0300

    feat: update project tt_um_oscillating_bones from urish/tt08-oscillating-bones
    Commit: 873f2b63960a6de37aa4d4e2a24d41e061d99851
    Workflow: https://github.com/urish/tt08-oscillating-bones/actions/runs/10722765464

---
+++ projects/tt_um_oscillating_bones/tt_um_oscillating_bones.v
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2024 Uri Shaked
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+`default_nettype none
+
+// just a stub to keep the Tiny Tapeout tools happy
+
+module tt_um_oscillating_bones (
+    input  wire       VGND,
+    input  wire       VDPWR,    // 1.8v power supply
+//    input  wire       VAPWR,    // 3.3v power supply
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+//    inout  wire [7:0] ua,       // Analog pins, only ua[5:0] can be used
+    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
+);
+
+endmodule
