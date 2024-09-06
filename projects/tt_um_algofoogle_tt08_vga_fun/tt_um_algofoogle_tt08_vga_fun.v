e28719229f92c23e2377573edf2ef7198b6b00a5
TinyTapeoutBot <139130078+TinyTapeoutBot@users.noreply.github.com>
2024-09-06 12:25:34 +0300

    feat: update project tt_um_algofoogle_tt08_vga_fun from algofoogle/tt08-vga-fun
    Commit: c20434647f8a03c4ca0dbd78a67f2f6fd285e5ed
    Workflow: https://github.com/algofoogle/tt08-vga-fun/actions/runs/10735855815

--- projects/tt_um_algofoogle_tt08_vga_fun/tt_um_algofoogle_tt08_vga_fun.v
+++ projects/tt_um_algofoogle_tt08_vga_fun/tt_um_algofoogle_tt08_vga_fun.v
@@ -102,28 +102,81 @@ module tt_um_algofoogle_tt08_vga_fun (
         // .Vpos       (ua[2])
     );
 
-    csdac_nom B_dac (
-        .vcc        (VDPWR),
-        .vss        (VGND),
-        .p0         (b[0]),
-        .n0         (bn[0]),
-        .p1         (b[1]),
-        .n1         (bn[1]),
-        .p2         (b[2]),
-        .n2         (bn[2]),
-        .p3         (b[3]),
-        .n3         (bn[3]),
-        .p4         (b[4]),
-        .n4         (bn[4]),
-        .p5         (b[5]),
-        .n5         (bn[5]),
-        .p6         (b[6]),
-        .n6         (bn[6]),
-        .p7         (b[7]),
-        .n7         (bn[7]),
-        .Vneg       (ua[3])
-        // .Vbias      (ua[1]),
-        // .Vpos       (ua[2])
+    wire sa1;
+    wire sa2;
+    wire sa3;
+    thermo2bit thermo_a (
+        .VCC(VDPWR),
+        .VSS(VGND),
+        .b0 (bn[0]),
+        .b1 (bn[1]),
+        .s1 (sa1),
+        .s2 (sa2),
+        .s3 (sa3)
+    );
+
+    wire sb1;
+    wire sb2;
+    wire sb3;
+    thermo2bit thermo_b (
+        .VCC(VDPWR),
+        .VSS(VGND),
+        .b0 (bn[2]),
+        .b1 (bn[3]),
+        .s1 (sb1),
+        .s2 (sb2),
+        .s3 (sb3)
+    );
+
+    wire sc1;
+    wire sc2;
+    wire sc3;
+    thermo2bit thermo_c (
+        .VCC(VDPWR),
+        .VSS(VGND),
+        .b0 (bn[4]),
+        .b1 (bn[5]),
+        .s1 (sc1),
+        .s2 (sc2),
+        .s3 (sc3)
+    );
+
+    wire sd1;
+    wire sd2;
+    wire sd3;
+    thermo2bit thermo_d (
+        .VCC(VDPWR),
+        .VSS(VGND),
+        .b0 (bn[6]),
+        .b1 (bn[7]),
+        .s1 (sd1),
+        .s2 (sd2),
+        .s3 (sd3)
+    );
+
+    wire bias1 = uio_in[5];
+    wire bias2 = uio_in[6];
+    wire bias3 = uio_in[7];
+    segdac segdac_0 (
+        .vcc(VDPWR),
+        .vss(VGND),
+        .sa1(sa1),
+        .sa2(sa2),
+        .sa3(sa3),
+        .sb1(sb1),
+        .sb2(sb2),
+        .sb3(sb3),
+        .sc1(sc1),
+        .sc2(sc2),
+        .sc3(sc3),
+        .sd1(sd1),
+        .sd2(sd2),
+        .sd3(sd3),
+        .bias1(bias1),
+        .bias2(bias2),
+        .bias3(bias3),
+        .Vout(ua[3])
+        //.Vbias()
     );
 
     // Configure uio directions...
@@ -135,9 +188,9 @@ module tt_um_algofoogle_tt08_vga_fun (
     assign uio_oe[2] = VGND;    // Input: UNUSED
     assign uio_oe[3] = VGND;    // Input: UNUSED
     assign uio_oe[4] = VGND;    // Input: UNUSED
-    assign uio_oe[5] = VGND;    // Input: UNUSED
-    assign uio_oe[6] = VGND;    // Input: UNUSED
-    assign uio_oe[7] = VGND;    // Input: UNUSED
+    assign uio_oe[5] = VGND;    // Input: bias1_in
+    assign uio_oe[6] = VGND;    // Input: bias2_in
+    assign uio_oe[7] = VGND;    // Input: bias3_in
 
     // Tie unused digital outputs, so they don't float:
     // assign uio_out[0] = hblank;
